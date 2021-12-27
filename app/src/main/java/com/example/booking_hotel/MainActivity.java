f1e8a8b5cb2353d8d5c3cd269b0893496645d4f7
Thuận Trần <thuantran190200@gmail.com>
2021-12-27 17:21:42 +0700

    common

--- app/src/main/java/com/example/booking_hotel/MainActivity.java
+++ app/src/main/java/com/example/booking_hotel/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.booking_hotel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.booking_hotel.intro.OnBoarding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 5000;
+    ImageView backgroud;
+    TextView title;
+
+    //Animations
+    Animation sideAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        //hooks
+        backgroud = findViewById(R.id.img_backgroud);
+        title = findViewById(R.id.txt_title);
+        //Animations
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+        //set Animations on elements
+        backgroud.setAnimation(sideAnim);
+        title.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, OnBoarding.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
     }
 }
