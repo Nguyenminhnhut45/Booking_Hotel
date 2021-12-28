5bb07a84b7162ee453c8ec21ae813bb6dad0c4ed
Thuận Trần <thuantran190200@gmail.com>
2021-12-28 21:49:14 +0700

    menu

--- app/src/main/java/com/example/booking_hotel/Home.java
+++ app/src/main/java/com/example/booking_hotel/Home.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Home extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/booking_hotel/Login.java
+++ app/src/main/java/com/example/booking_hotel/Login.java
@@ -83,7 +83,7 @@ public class Login extends AppCompatActivity {
         btn_quenmk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
     }
