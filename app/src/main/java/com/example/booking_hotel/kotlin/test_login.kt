393b8588c586f3d410920a4396e55139c1fa94af
Thuận Trần <thuantran190200@gmail.com>
2021-12-31 16:03:00 +0700

    update

---
+++ app/src/main/java/com/example/booking_hotel/kotlin/test_login.kt
@@ -0,0 +1,18 @@
+package com.example.booking_hotel.kotlin
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.booking_hotel.R
+
+class test_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test);
+
+        dangky1
+
+
+
+    }
+}
